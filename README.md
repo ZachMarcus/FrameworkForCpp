d1eb4456891b1c0dcc284d6a75114a726016ad19
ZachMarcus <zachary.j.marcus@gmail.com>
2017-08-12 23:00:45 +0200

    Restructured some dependencies and built out optparse usage

--- README.md
+++ README.md
@@ -2,25 +2,26 @@
 A basic starting point for C++ Linux projects
 
 I never seem to add in this functionality into projects until I'm far in.
+
 If starting with all of that preconfigured and set up makes that easy, then this structure is for you.
 
-This project contains:
+## This project contains:
  - A CMake structure for out of source builds
  - A full project layout with the structure for tests, src, include
- - A Sample optparse usage in Main.cpp
- - Links to Config4cpp
- - Links to Log4cxx
- - Inclusion of DocTest
+ - A Sample [optparse](https://github.com/myint/optparse "Python's optparse but C++ and header-only") usage in Main.cpp
+ - Inclusion of [Config4cpp](http://www.config4star.org/ "Config4*'s homepage")
+ - Inclusion of [Log4cxx](https://logging.apache.org/log4cxx "Apache Log4cxx's homepage)
+ - Inclusion of [DocTest](https://github.com/onqtam/doctest "Doctest's github homepage")
     - Note that the following line should only occur once per binary:
-
+```
     #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
+```
     - Then include the following in each source file:
-
+```
     #include "doctest.h"
+```
 
-
-# Project Dependencies:
+## Project Dependencies:
 
  - apr-1.x
  - apr-utils-1.x
@@ -32,8 +33,8 @@ This project contains:
  - log4cxx
 
 
-For Debian (tested on Ubuntu 16.04), try
-
+### For Debian (tested on Ubuntu 16.04), try
+```
     sudo apt install libapr1-dev libaprutil1-dev zip gzip libtool autotools-dev automake liblog4cxx-dev
-
+```
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,68 @@
 
-#include "Project.h"
 #include <iostream>
 
+
+#include "Project.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "config4cpp/ConfigurationException.h"
+#include "config4cpp/Configuration.h"
+#include "log4cxx/logger.h"
+#include "optparse.h"
+
+
 int main(int argc, char** argv) {
+
+    // Doctest boilerplate
+    doctest::Context context;
+    context.addFilter("test-case-exclude", "*math"); // exclude test cases with "math" in their name
+    context.setOption("abort-after", 5); // stop test execution after 5 failed assertions
+    context.setOption("order-by", "name"); // sort the test cases by their name
+    context.applyCommandLine(argc, argv);
+    // Doctest overrides
+    context.setOption("no-breaks", true); // don't break in the debugger when asserts fail
+    // Doctest boilerplate
+    int doctestReturn = context.run(); // run tests
+    if (context.shouldExit()) { // important - query flags (and --exit) rely on doing this
+        return doctestReturn; // propagate the result of the tests
+    }
+
+    // OptParse usage
+    optparse::OptionParser parser = optparse::OptionParser().description("Main.cpp argument parser");
+    parser.add_option("-cf", "--configFile")
+          .dest("configFile")
+          .action("store")
+          .type("string")
+          .set_default("./config/MainConfig.cfg")
+          .help("filepath to config FILE to be parsed by config4cpp")
+          .metavar("FILE");
+    parser.add_option("-lf", "--logFile")
+          .dest("logFile")
+          .action("store")
+          .type("string")
+          .set_default("./config/Log4Cxx.cfg")
+          .help("filepath to log config FILE to be parsed by Log4Cxx")
+          .metavar("FILE");
+
+    optparse::Values options = parser.parse_args(argc, argv);
+    std::vector<std::string> parsedArgs = parser.args();
+
+    for (auto i : parsedArgs) {
+        std::cout << i << std::endl;
+    }
+
+
+    if (options.get("configFile")) {
+        std::cout << "ConfigFile: " << options["configFile"] << std::endl;
+    }
+    if (options.get("logFile")) {
+        std::cout << "LogFile: " << options["logFile"] << std::endl;
+    }
+
+
+    int projectReturn = 0;
+
     std::cout << "Hello world!" << std::endl;
-    return 0;
+    return doctestReturn + projectReturn;
 }
